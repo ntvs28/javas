9ed15530694a34341387f25673c6aa02423eac69
ntvs28 <ntvsagar@gmail.com>
2024-02-16 14:59:47 +0530

    first commit

---
+++ a2.java
@@ -0,0 +1,8 @@
+
+public class a2{
+    public static void main(String[] args) {
+        System.out.println("Nama = Rosita Melani Lumenta");
+        System.out.println("NPM = 195110011");
+        System.out.println("Jurusan = Informatika");
+    }
+}
